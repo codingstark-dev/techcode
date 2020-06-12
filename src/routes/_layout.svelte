fde08b61139bf7ab95dcbab1c5b5c7c9cb4ee158
Himanshu Maurya <mhimanshu.338@gmail.com>
2020-06-12 21:13:49 +0530

    first commit

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,18 +1,34 @@
 <script>
-	import Nav from '../components/Nav.svelte';
+  import Footer from "./../components/footer.svelte";
+  import Header from "./../components/header.svelte";
+  import Tailwindcss from "../components/Tailwindcss.svelte";
+  import Nav from "../components/Nav.svelte";
 
-	export let segment;
+  // export let segment;
 </script>
 
 <style>
-	main {
-		@apply relative bg-white p-8 max-w-4xl mx-auto my-0;
-		box-sizing: border-box;
-	}
+  main {
+    position: relative;
+    max-width: 56em;
+    /* background-color: white; */
+    padding: 1em;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
 </style>
 
-<Nav {segment}/>
 
-<main>
-	<slot></slot>
-</main>
+<!-- <Nav {segment}/> -->
+
+<Header />
+<div class="bg-indigo-500 w-auto h-64 rounded-b-md">
+
+  <main class="pt-3">
+    <slot />
+  </main>
+</div>
+
+<div class="h-64" />
+<div class="h-20" />
+<Footer />
