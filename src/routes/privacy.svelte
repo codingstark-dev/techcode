aaf8d92512a0bc8d3323a35d46c06ee314b191c7
Himanshu Maurya <40685364+refoxdev@users.noreply.github.com>
2020-07-09 02:25:58 +0530

    file changed

--- src/routes/privacy.svelte
+++ src/routes/privacy.svelte
@@ -7,6 +7,7 @@
     "ad support",
     "commercial"
   ];
+  let selected = "dd";
 </script>
 
 <div class="space-y-3">
@@ -17,8 +18,26 @@
     text={'Personally Identifiable Information'}
     placeholder={'Personally Identifiable Information you collect (comma separated)'} />
 </div>
-<select>
-  {#each apptype as d}
-    <option >{d}</option>
-  {/each}
-</select>
+<br />
+<div class="space-y-3">
+  <p class="pl-1 font-semibold">App Type</p>
+  <select
+    class="bg-white focus:outline-none border border-gray-300 rounded-lg py-2
+    px-4 block w-auto appearance-none leading-normal shadow-sm"
+    value={selected}
+    on:change={a => (selected = a)}>
+    {#each apptype as d}
+      <option value={d}>{d}</option>
+    {/each}
+  </select>
+  <p class="pl-1 font-semibold">App Type</p>
+  <select
+    class="bg-white focus:outline-none border border-gray-300 rounded-lg py-2
+    px-4 block w-auto appearance-none leading-normal shadow-sm"
+    value={selected}
+    on:change={a => (selected = a)}>
+    {#each apptype as d}
+      <option value={d}>{d}</option>
+    {/each}
+  </select>
+</div>
--- src/routes/video-downloader.svelte
+++ src/routes/video-downloader.svelte
@@ -1,171 +1,154 @@
 <script>
-  import Decs from "./../shared/Decs.svelte";
-
-  import StoreData from "./../store.js";
-  import Loading from "./../components/loading.svelte";
-  import { onMount } from "svelte";
-
-  const apiUrl = "krd6o6BAmZovoVhIe3RYjBh1mVos3tsxwWsTxdyIPE8a7RbwxZ";
-  const urlss =
-    "https://cors-anywhere.herokuapp.com/http://keepsaveit.com/api/?api_key=";
-  let pinurl = `https://pin.it/1y4y6ET`;
-  let success = "success";
-  let loading = false;
-
-  const userAction = async () => {
-    loading = true;
-    StoreData.set();
-    const response = await fetch(`${urlss}${apiUrl}&url=${pinurl}`, {
-      method: "GET",
-      // body: myBody, // string or object
-      headers: {
-        "Content-Type": "application/json"
-      }
-    }).finally(() => (loading = false));
-    const myJson = await response.json();
-    // console.log(myJson);
-    StoreData.update(() => {
-      return myJson;
-    });
-
-    // return loading = false;
-    // console.log($StoreData);
-  };
-
-  // onMount(userAction);
+  import Card from "./../components/card.svelte";
 </script>
 
-<style>
-  .error {
-    text-align: center;
-    font-size: 15px;
-    color: red;
-  }
-  .lol {
-    padding-bottom: 10px;
-  }
-</style>
+<div
+  class="flex flex-wrap mx-2 overflow-hidden sm:-mx-2 md:-mx-2 lg:-mx-2 xl:-mx-2">
 
-<!-- <div class="link pt-3 ">
-  <div class="input-group mb-3 w-auto px-md-5 ">
-    <input type="text" class="form-control" aria-describedby="button-addon2" />
-    <div class="input-group-append">
-      <button
-        class="btn btn-outline-danger"
-        type="button"
-        id="button-addon2"
-        on:click={userAction}>
-        Get
-      </button>
-    </div>
+  <div
+    class="my-2 px-2 w-1/2 overflow-hidden sm:my-2 sm:px-2 sm:w-1/3 md:my-2
+    md:px-2 md:w-1/4 lg:my-2 lg:px-2 lg:w-1/4 xl:my-2 xl:px-2 xl:w-1/4">
+    <!-- Column Content -->
+    <Card links={'pincode-finder-india'}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="fill-current text-indigo-500 hover:text-indigo-400 w-24 h-24 mx-4"
+        viewBox="0 0 24 24">
+        <path
+          d="M12 0c-6.627 0-12 5.372-12 12 0 5.084 3.163 9.426 7.627
+          11.174-.105-.949-.2-2.405.042-3.441.218-.937 1.407-5.965
+          1.407-5.965s-.359-.719-.359-1.782c0-1.668.967-2.914 2.171-2.914 1.023
+          0 1.518.769 1.518 1.69 0 1.029-.655 2.568-.994 3.995-.283 1.194.599
+          2.169 1.777 2.169 2.133 0 3.772-2.249 3.772-5.495
+          0-2.873-2.064-4.882-5.012-4.882-3.414 0-5.418 2.561-5.418 5.207 0
+          1.031.397 2.138.893 2.738.098.119.112.224.083.345l-.333
+          1.36c-.053.22-.174.267-.402.161-1.499-.698-2.436-2.889-2.436-4.649
+          0-3.785 2.75-7.262 7.929-7.262 4.163 0 7.398 2.967 7.398 6.931 0
+          4.136-2.607 7.464-6.227 7.464-1.216 0-2.359-.631-2.75-1.378l-.748
+          2.853c-.271 1.043-1.002 2.35-1.492 3.146 1.124.347 2.317.535 3.554.535
+          6.627 0 12-5.373 12-12 0-6.628-5.373-12-12-12z"
+          fill-rule="evenodd"
+          clip-rule="evenodd" />
+      </svg>
+
+      <div class="mt-2">
+        <span
+          class="font-bold text-base font-sans text-indigo-500
+          hover:text-indigo-400">
+          Pinterest
+        </span>
+      </div>
+    </Card>
   </div>
-</div> -->
-<div class="flex pt-3">
-  <input
-    class="bg-white focus:outline-none focus:shadow-outline border
-    border-gray-300 rounded-md py-2 px-4 block w-full appearance-none
-    leading-normal"
-    type="text"
-    placeholder="Paster Your Link"
-    aria-label="Paster Your Link"
-    bind:value={pinurl} />
-  <button
-    class="bg-white hover:bg-indigo-500 text-gray-800 font-semibold py-2 px-4
-    border border-indigo-500 rounded shadow hover:text-white"
-    on:click={userAction}>
-    Get
-  </button>
+
+  <div
+    class="my-2 px-2 w-1/2 overflow-hidden sm:my-2 sm:px-2 sm:w-1/3 md:my-2
+    md:px-2 md:w-1/4 lg:my-2 lg:px-2 lg:w-1/4 xl:my-2 xl:px-2 xl:w-1/4">
+    <!-- Column Content -->
+    <Card>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class=" w-20 h-20 mx-6 fill-current text-indigo-500
+        hover:text-indigo-400 pt-3"
+        viewBox="0 0 24 24">
+        <path
+          d="M14.829
+          6.302c-.738-.034-.96-.04-2.829-.04s-2.09.007-2.828.04c-1.899.087-2.783.986-2.87
+          2.87-.033.738-.041.959-.041 2.828s.008 2.09.041 2.829c.087 1.879.967
+          2.783 2.87 2.87.737.033.959.041 2.828.041 1.87 0 2.091-.007 2.829-.041
+          1.899-.086 2.782-.988
+          2.87-2.87.033-.738.04-.96.04-2.829s-.007-2.09-.04-2.828c-.088-1.883-.973-2.783-2.87-2.87zm-2.829
+          9.293c-1.985 0-3.595-1.609-3.595-3.595 0-1.985 1.61-3.594
+          3.595-3.594s3.595 1.609 3.595 3.594c0 1.985-1.61 3.595-3.595
+          3.595zm3.737-6.491c-.464 0-.84-.376-.84-.84 0-.464.376-.84.84-.84.464
+          0 .84.376.84.84 0 .463-.376.84-.84.84zm-1.404 2.896c0 1.289-1.045
+          2.333-2.333 2.333s-2.333-1.044-2.333-2.333c0-1.289 1.045-2.333
+          2.333-2.333s2.333 1.044 2.333 2.333zm-2.333-12c-6.627 0-12 5.373-12
+          12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.958 14.886c-.115
+          2.545-1.532 3.955-4.071
+          4.072-.747.034-.986.042-2.887.042s-2.139-.008-2.886-.042c-2.544-.117-3.955-1.529-4.072-4.072-.034-.746-.042-.985-.042-2.886
+          0-1.901.008-2.139.042-2.886.117-2.544 1.529-3.955
+          4.072-4.071.747-.035.985-.043 2.886-.043s2.14.008 2.887.043c2.545.117
+          3.957 1.532 4.071 4.071.034.747.042.985.042 2.886 0 1.901-.008
+          2.14-.042 2.886z" />
+      </svg>
+
+      <div class="mt-2">
+        <span
+          class="font-bold text-base font-sans text-indigo-500
+          hover:text-indigo-400">
+          Instagram
+        </span>
+      </div>
+    </Card>
+  </div>
+
+  <div
+    class="my-2 px-2 w-1/2 overflow-hidden sm:my-2 sm:px-2 sm:w-1/3 md:my-2
+    md:px-2 md:w-1/4 lg:my-2 lg:px-2 lg:w-1/4 xl:my-2 xl:px-2 xl:w-1/4">
+    <!-- Column Content -->
+    <Card links={'video-downloader'}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class=" w-20 h-20 mx-6 fill-current text-indigo-500
+        hover:text-indigo-400 pt-3"
+        viewBox="0 0 24 24">
+        <path
+          d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373
+          12-12-5.373-12-12-12zm6.066 9.645c.183 4.04-2.83 8.544-8.164
+          8.544-1.622 0-3.131-.476-4.402-1.291 1.524.18 3.045-.244
+          4.252-1.189-1.256-.023-2.317-.854-2.684-1.995.451.086.895.061
+          1.298-.049-1.381-.278-2.335-1.522-2.304-2.853.388.215.83.344
+          1.301.359-1.279-.855-1.641-2.544-.889-3.835 1.416 1.738 3.533 2.881
+          5.92 3.001-.419-1.796.944-3.527 2.799-3.527.825 0 1.572.349
+          2.096.907.654-.128 1.27-.368 1.824-.697-.215.671-.67 1.233-1.263
+          1.589.581-.07 1.135-.224 1.649-.453-.384.578-.87 1.084-1.433 1.489z" />
+      </svg>
+
+      <div class="mt-2">
+        <span
+          class="font-bold text-base font-sans text-indigo-500
+          hover:text-indigo-400">
+          Twitter
+        </span>
+      </div>
+    </Card>
+  </div>
+
+  <div
+    class="my-2 px-2 w-1/2 overflow-hidden sm:my-2 sm:px-2 sm:w-1/3 md:my-2
+    md:px-2 md:w-1/4 lg:my-2 lg:px-2 lg:w-1/4 xl:my-2 xl:px-2 xl:w-1/4">
+    <!-- Column Content -->
+
+    <Card>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+       class=" w-20 h-20 mx-6 fill-current text-indigo-500
+        hover:text-indigo-400 pt-3"
+        viewBox="0 0 24 24">
+        <path
+          d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373
+          12-12-5.373-12-12-12zm5.158 17.897c-.459.604-2.004 1.103-3.403
+          1.103-3.482
+          0-5.255-1.283-5.255-4.001v-3.592h-1.691l-.059-.059v-2.764l.039-.056.156-.052c1.398-.466
+          2.296-1.252
+          2.494-2.873.049-.392.377-.603.663-.603h4.172l.058.059v2.81l.058.059h2.219l.06.059v3.363l-.06.059h-2.229l-.06.059v3.332c.002.09.006.362.096.362h1.909l.057.04.868
+          2.574-.092.121zm-1.145-1.994l.581
+          1.721-.014.062c-.438.421-1.49.733-2.35.748l-.098.001c-2.828
+          0-3.313-2.16-3.313-3.435v-4.106l-.059-.059h-1.622l-.059-.059-.014-1.786.037-.054c1.462-.569
+          2.278-1.514
+          2.492-3.263.013-.097.093-.1.094-.1h2.011l.059.059v2.81l.059.059h2.219l.058.059v2.217l-.058.059h-2.229l-.057.059v3.911c.014.839.417
+          1.265 1.205 1.265.316 0 .65-.074.982-.201l.076.033z" />
+      </svg>
+    
+      <div class="mt-2">
+        <span
+          class="font-bold text-base font-sans text-indigo-500
+          hover:text-indigo-400">
+         Tumblr 
+        </span>
+      </div>
+    </Card>
+  </div>
+
 </div>
-<br />
-<!-- <div class="text-center">
-  <h1>wait</h1>
-</div> -->
-
-{#if loading === true}
-  <Loading />
-{:else if $StoreData === undefined}
-  <Decs />
-{:else if $StoreData.status === false || $StoreData.code == 112 || $StoreData.code == 405 || $StoreData.code == 404 || $StoreData.code == 113 || $StoreData.code == 212}
-  <h1 class="error">{$StoreData.msg}</h1>
-{:else}
-  {#each $StoreData['response']['links'] as item}
-    {#if item.ext == 'mp3' || (item.ext == 'mp4' && item.size != '0 bytes')}
-      <div class="w-11/12 mx-auto min-h-full">
-        <div
-          class="bg-white shadow-md rounded text-center "
-          style="width: 100%;">
-          <div class="text-center">
-            <video width="350" height="300" controls>
-              <source src={item['url']} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-          </div>
-
-          <h5 class=" text-center font-semibold pt-3 text-lg">
-            {$StoreData['response']['title']}
-          </h5>
-
-          <table class="text-left w-full border-collapse min-h-full">
-            <thead>
-              <tr>
-                <th
-                  class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm
-                  text-grey-dark border-b border-grey-light">
-                  Format
-                </th>
-                <th
-                  class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm
-                  text-grey-dark border-b border-grey-light">
-                  Resolution
-                </th>
-                <th
-                  class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm
-                  text-grey-dark border-b border-grey-light">
-                  Size
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td
-                  class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm
-                  text-grey-dark border-b border-grey-light">
-                  {item.ext}
-                </td>
-                <td
-                  class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm
-                  text-grey-dark border-b border-grey-light">
-                  {item.resolution}
-                </td>
-                <td
-                  class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm
-                  text-grey-dark border-b border-grey-light">
-                  {item.size}
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <a href={item['url']} target="_blank">
-            <button
-              type="button"
-              class="bg-white hover:bg-indigo-500 text-gray-800 font-semibold
-              my-2 py-2 px-4 border border-indigo-500 rounded shadow hover:text-white">
-              Download
-            </button>
-          </a>
-
-        </div>
-      </div>
-    {/if}
-
-    <!-- <div class=" embed-responsive-4by3 shadow-sm">
-        <video width="350" height="300" controls>
-          <source src={item['url']} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
-      <a href={item['url']} download>
-        <button type="button" class="btn btn-success">Download</button>
-      </a> -->
-  {/each}
-{/if}
